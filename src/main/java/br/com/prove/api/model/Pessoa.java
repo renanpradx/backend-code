5996bfabe1bed5136eb7e4e4a90aac4f002b34bc
JeffMascarenhas <126513595+JeffMascarenhas@users.noreply.github.com>
2023-02-27 17:44:57 -0300

    Adicionou novo Model (Pessoa)

---
+++ src/main/java/br/com/prove/api/model/Pessoa.java
@@ -0,0 +1,29 @@
+package br.com.prove.api.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_pessoa", schema = "svc_rh_prove")
+public class Pessoa {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_pessoa")
+    private Long id;
+
+    @Column(name = "nm_pessoa")
+    private String nome;
+
+    @Column(name = "idade_pessoa")
+    private Long idade;
+
+
+}
